c8fd26ce16d876ba4576c01e8cdf2c0e4a93a88b
akmdi <amitkumar.mdi@gmail.com>
2022-04-05 14:47:01 +0530

    wip

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,10 @@ export default function Index() {
           <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
             <div className="pt-32 sm:pt-0">
               <h2 className="font-semibold text-4xl text-blueGray-600">
-                UX4G - User Experience for Government
+                UX4G - User Experience
+              </h2>
+              <h2 className="font-semibold text-4xl text-blueGray-600">
+                for Government
               </h2>
               <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
                 UX4G features multiple HTML elements and it comes with dynamic
